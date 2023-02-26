a4a47582edc7b33b73da6d86d26bf65d9639cca2
AnnaXT <annateng8000@gmail.com>
2023-02-26 03:19:50 -0500

    Added Enemies and Spike Trap
    Enemy types:
    1 - NavMesh constant follow
    2 - not NavMesh phases through walls
    3 - NavMesh starts following once triggered
    
    Added scripts for spike trap animations

---
+++ Ghosts and Goblins Galore/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System;
+
+public class Player : MonoBehaviour
+{
+    NavMeshAgent _newNavMeshAgent;
+    Camera mainCam;
+
+    void Start()
+    {
+        _newNavMeshAgent = GetComponent<NavMeshAgent>();
+        mainCam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(mainCam.ScreenPointToRay(Input.mousePosition), out hit, 200))
+            {
+                // print(hit.point);
+                _newNavMeshAgent.destination = hit.point;
+            }
+        }
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Key"))
+    //    {
+    //        int keyNum = Int32.Parse(other.name.Substring(3)); // all key must be named "Key" andm numner "Key0"
+    //        Destroy(other.gameObject);
+    //        PublicVars.hasKey[keyNum] = true;
+    //    }
+}
+
