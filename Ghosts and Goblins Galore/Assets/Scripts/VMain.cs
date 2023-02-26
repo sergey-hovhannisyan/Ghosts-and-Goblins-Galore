29ebebb2a1ecd9ecceb0d3a244ba9591be0ac17d
xxiaoling <xz3490@nyu.edu>
2023-02-26 04:28:36 -0500

    added end scene and CameraFollow Script
    victory scene
    confetti prefab
    script for victory scene
    script for camera follow player

---
+++ Ghosts and Goblins Galore/Assets/Scripts/VMain.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VMain : MonoBehaviour
+{
+    public AudioClip fwSnd;
+    public GameObject confetti;
+    AudioSource _audioSource;
+
+
+    IEnumerator Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        StartCoroutine(StopSound());
+        InvokeRepeating("PlaySound", 1, 1);
+
+        yield return new WaitForSeconds(9);
+        Vector2 spawnPos = new Vector2(0, 10);
+        Instantiate(confetti, spawnPos, Quaternion.identity);
+        Instantiate(confetti, spawnPos, Quaternion.identity);
+    }
+
+
+    void PlaySound()
+    {
+        _audioSource.PlayOneShot(fwSnd);
+    }
+
+
+    IEnumerator StopSound(){
+        yield return new WaitForSeconds(10);
+        CancelInvoke();
+    }
+}
